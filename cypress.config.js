9bbc1edf5ede30d8accf075dfda37636920dc365
Fernando Fagundes de  Andrade <fernandofagunde@gmail.com>
2026-01-22 15:36:48 -0300

    testeddd

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9090',
+    baseUrl: "http://localhost:9090",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config);
+      return config;
     },
   },
 });
