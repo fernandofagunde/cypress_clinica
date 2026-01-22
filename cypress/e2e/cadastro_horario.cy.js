2cc458659b9c438d52b6fada26fb9dbdca70cc90
Fernando Fagundes de  Andrade <fernandofagunde@gmail.com>
2026-01-22 16:47:59 -0300

    testeddd

--- cypress/e2e/cadastro_horario.cy.js
+++ cypress/e2e/cadastro_horario.cy.js
@@ -1,4 +1,4 @@
-import { getDataHojeBR } from "../support/utils/data";
+import { getDataHojeISO } from "../support/utils/data";
 describe("Cadastro Horário", () => {
   beforeEach(() => {
     cy.visit("/login");
@@ -7,12 +7,18 @@ describe("Cadastro Horário", () => {
     cy.login("secretaria", "37237180");
     cy.get(":nth-child(1) > .nav-link").should("be.visible").click();
 
-    cy.get('[name="dataInicio"]')
-      .invoke("val", getDataHojeBR(1))
-      .trigger("change");
-    cy.get('[name="dataFim"]')
-      .invoke("val", getDataHojeBR(1))
-      .trigger("change");
+    // cy.get('[name="dataInicio"]')
+    //   .invoke("val", getDataHojeBR(1))
+    //    .trigger("change");
+
+    cy.get('[name="dataInicio"]').then(($el) => {
+      const fp = $el[0]._flatpickr;
+      fp.setDate(getDataHojeISO(1), true);
+    });
+    cy.get('[name="dataFim"]').then(($el) => {
+      const fp = $el[0]._flatpickr;
+      fp.setDate(getDataHojeISO(1), true);
+    });
     cy.get('[name="horaInicio"]').select("08:00");
     cy.get('[name="horaFim"]').select("10:00");
     cy.get(".btn-submit").click();
--- cypress/support/utils/data.js
+++ cypress/support/utils/data.js
@@ -1,6 +1,10 @@
-export function getDataHojeBR(dias = 0) {
+export function getDataHojeISO(dias = 0) {
   const data = new Date();
   data.setDate(data.getDate() + dias);
 
-  return data.toLocaleDateString("pt-BR");
+  const ano = data.getFullYear();
+  const mes = String(data.getMonth() + 1).padStart(2, "0");
+  const dia = String(data.getDate()).padStart(2, "0");
+
+  return `${ano}-${mes}-${dia}`;
 }
